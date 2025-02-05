9408b6585fba9aebf765e0d02604a064d3b276f6
Adio03 <adioldmj@gmail.com>
2025-02-05 10:50:00 +0100

    add docker file

--- src/main/java/com/example/hng12_stage0/service/InformationImpl.java
+++ src/main/java/com/example/hng12_stage0/service/InformationImpl.java
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 @Service
 public class InformationImpl implements InformationService {
     @Override
     public Map<String, String> getInformation() {
-        Map<String, String> response = new HashMap<>();
+        Map<String, String> response = new LinkedHashMap<>();
         response.put("email", "adioldmj@gmail.com");
         response.put("current_datetime", Instant.now().atOffset(ZoneOffset.UTC).truncatedTo(java.time.temporal.ChronoUnit.SECONDS).format(DateTimeFormatter.ISO_INSTANT));
         response.put("github_url", "https://github.com/Adio03/hng12_stage0.git");
